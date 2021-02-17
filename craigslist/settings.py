9a49a36607205a9b241f0779f7e69748c2c93f61
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-02-17 10:40:18 +0300

    get ready for heroku

--- craigslist/settings.py
+++ craigslist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,3 +117,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
+
